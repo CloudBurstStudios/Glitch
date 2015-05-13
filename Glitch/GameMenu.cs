3d9b6a227eca98d25d2aa7f93b416c7a4ecabbb6
cbrown3 <cbrow2014@gmail.com>
2015-05-13 14:14:34 -0400

    Game Menu Changes

--- Glitch/GameMenu.cs
+++ Glitch/GameMenu.cs
@@ -24,63 +24,6 @@ namespace Glitch
             get { return score; }
             set { score = value; }
         }
-        /*public void UpdateMenu()
-        {
-            kState = Keyboard.GetState();
-            dir[0] = kState.IsKeyDown(Keys.Up);
-            dir[1] = kState.IsKeyDown(Keys.Down);
-            if (y1 == 200)
-            {
-                if (dir[1])
-                {
-                    x1 = 280;
-                    x2 = 485;
-                    y1 = 275;
-                    y2 = 325;
-                    color1 = Color.Black;
-                    color2 = Color.White;
-                    Thread.Sleep(100);
-                }
-            }
-
-            else if (y1 == 275)
-            {
-                if (dir[0])
-                {
-                    x1 = 280;
-                    x2 = 485;
-                    y1 = 200;
-                    y2 = 240;
-                    color2 = Color.Black;
-                    color1 = Color.White;
-                    Thread.Sleep(100);
-                }
-                else if (dir[1])
-                {
-                    x1 = 280;
-                    x2 = 485;
-                    y1 = 350;
-                    y2 = 400;
-                    color2 = Color.Black;
-                    color3 = Color.White;
-                    Thread.Sleep(100);
-                }
-            }
-
-            else if (y1 == 350)
-            {
-                if (dir[0])
-                {
-                    x1 = 280;
-                    x2 = 485;
-                    y1 = 275;
-                    y2 = 325;
-                    color3 = Color.Black;
-                    color2 = Color.White;
-                    Thread.Sleep(100);
-                }
-            }
-        }*/
 
         public void DrawText(SpriteBatch spriteBatch, SpriteFont font)
         {
@@ -95,16 +38,6 @@ namespace Glitch
                 1f,
                 spritEffects,
                 0);
-
-            /*spriteBatch.DrawString(font,
-                "Laser Charge:",
-                new Vector2(25, 400),
-                Color.Black,
-                0f,
-                new Vector2(0, 0),
-                1f,
-                spritEffects,
-                0);*/
 
             spriteBatch.DrawString(font,
                 "Score: " + score,
